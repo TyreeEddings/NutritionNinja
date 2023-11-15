3dc7036000eda0bdd06326171c4fc7dc27520582
SWAMMBR54 <124000234+SWAMMBR54@users.noreply.github.com>
2023-11-15 12:27:04 -0600

    Update charts.js
    added temp charts

--- goals/charts.js
+++ goals/charts.js
@@ -1,4 +1,4 @@
-const cal = document.getElementById("calchart");
+const cal = document.getElementById("daycalchart");
 
 new Chart(cal, {
     type: "bar",
@@ -6,29 +6,49 @@ new Chart(cal, {
         labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5"],
         datasets: [{
             label: "Caloric Intake",
-            data: [2000, 2456, 2301, 1011, 1567],
+            data: [2501, 2234, 1256, 1633, 2289],
             borderWidth: 1
         }]
     },
     options: {
         scales: {
             y: {
-                beginAtZero: true
+                beginAtZero: true,
+                responsive: false
             }
         }
     }
 });
 
-const dist = document.getElementById("nutridist")
+const dist = document.getElementById("nutridistchart")
 
 new Chart(dist, {
     type: "pie",
     data: {
-        labels: ["Carbs", "Fats", "Protiens", "Vitamins/Minerals", "Other"],
+        labels: ["Carbs", "Proteins", "Fats"],
         datasets: [{
             label: "Nutrient Distribution",
             title: "Nutrient Distribution",
-            data: [20, 40, 10, 15, 15],
+            data: [30, 50, 20],
         }]
     },
+    options: {
+        responsive: false
+    }
+});
+
+const avgdist = document.getElementById("avgdistchart")
+
+new Chart(avgdist, {
+    type: "pie",
+    data: {
+        labels: ["Carbs", "Proteins", "Fats"],
+        datasets: [{
+            label: "Average Nutrient Distribution",
+            data: [55, 25, 20]
+        }]
+    },
+    options: {
+        responsive: false
+    }
 });
