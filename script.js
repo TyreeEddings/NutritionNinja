457784d288d64ab73095dd84bdcb84bc730d9600
TyreeEddings <97616579+TyreeEddings@users.noreply.github.com>
2023-11-16 21:38:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const sendChatBtn = document.querySelector(".chat-input span");
 
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "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"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 
