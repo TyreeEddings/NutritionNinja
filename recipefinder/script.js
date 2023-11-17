3a32edadec791b19ed02f2ade6564ed404780ca9
TyreeEddings <97616579+TyreeEddings@users.noreply.github.com>
2023-11-16 21:38:33 -0600

    Update script.js

--- recipefinder/script.js
+++ recipefinder/script.js
@@ -72,7 +72,7 @@ const sendChatBtn = document.querySelector(".chat-input span");
 
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "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"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 
