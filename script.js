577f8bdac288b63e511ec213123365e8fc529133
DavidZ6 <150367556+DavidZ6@users.noreply.github.com>
2023-11-16 21:55:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const sendChatBtn = document.querySelector(".chat-input span");
 
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "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"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 
